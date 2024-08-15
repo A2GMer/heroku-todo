3b8d2ff00d33cdaa5db04c1760f0dc752be14a78
A2GMer <52153162+A2GMer@users.noreply.github.com>
2024-08-16 00:19:42 +0900

    modify

--- todolist/todolist/settings.py
+++ todolist/todolist/settings.py
@@ -13,14 +13,14 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-(-j_enu)f%%(390ck+-$t1oi##8x)9%&0)pe)-_y=c!*p0r%=('
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
